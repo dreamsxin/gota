03fb2ffd1a0801b0d82bfcfb96547b0f7f5ad5a7
zhuzongxin <dreamsxin@qq.com>
2024-03-22 10:06:55 +0800

    Rename mat::Dot to mat::Mul

--- mat/matrix.go
+++ mat/matrix.go
@@ -4,7 +4,70 @@ import (
 	"github.com/dreamsxin/gota/series"
 )
 
-func Dot(a series.Series, b series.Series) series.Series {
+func Mul(a series.Series, b series.Series) series.Series {
+	var c series.Series
+	la := a.Len()
+	lb := b.Len()
+	if a.Type() != series.Int || b.Type() != series.Int {
+
+		c = series.Floats([]float64{})
+		if la >= lb {
+			av := a.Float()
+			for i, v := range b.Float() {
+				c.Append(av[i] * v)
+			}
+		} else {
+			bv := a.Float()
+			for i, v := range a.Float() {
+				c.Append(v * bv[i])
+			}
+		}
+	} else {
+		c = series.Ints([]int64{})
+		if la >= lb {
+			av := a.Int64()
+			for i, v := range b.Int64() {
+				c.Append(av[i] * v)
+			}
+		} else {
+			bv := a.Int64()
+			for i, v := range a.Int64() {
+				c.Append(v * bv[i])
+			}
+		}
+	}
+	return c
+}
+
+func Div(a series.Series, b series.Series) series.Series {
+	var c series.Series
+	la := a.Len()
+	lb := b.Len()
+
+	c = series.Floats([]float64{})
+	if la >= lb {
+		av := a.Float()
+		for i, v := range b.Float() {
+			if v != 0 {
+				c.Append(av[i] / v)
+			} else {
+				c.Append(0)
+			}
+		}
+	} else {
+		bv := a.Float()
+		for i, v := range a.Float() {
+			if v != 0 {
+				c.Append(v * bv[i])
+			} else {
+				c.Append(0)
+			}
+		}
+	}
+	return c
+}
+
+func Sub(a series.Series, b series.Series) series.Series {
 	var c series.Series
 	la := a.Len()
 	lb := b.Len()
@@ -14,12 +77,12 @@ func Dot(a series.Series, b series.Series) series.Series {
 		if la >= lb {
 			av := a.Float()
 			for i, v := range b.Float() {
-				c.Append(v * av[i])
+				c.Append(av[i] - v)
 			}
 		} else {
 			bv := a.Float()
 			for i, v := range a.Float() {
-				c.Append(v * bv[i])
+				c.Append(v - bv[i])
 			}
 		}
 	default:
@@ -27,12 +90,47 @@ func Dot(a series.Series, b series.Series) series.Series {
 		if la >= lb {
 			av := a.Int64()
 			for i, v := range b.Int64() {
-				c.Append(v * av[i])
+				c.Append(av[i] - v)
 			}
 		} else {
 			bv := a.Int64()
 			for i, v := range a.Int64() {
-				c.Append(v * bv[i])
+				c.Append(v - bv[i])
+			}
+		}
+	}
+	return c
+}
+
+func Add(a series.Series, b series.Series) series.Series {
+	var c series.Series
+	la := a.Len()
+	lb := b.Len()
+	switch a.Type() {
+	case series.Float:
+		c = series.Floats([]float64{})
+		if la >= lb {
+			av := a.Float()
+			for i, v := range b.Float() {
+				c.Append(av[i] + v)
+			}
+		} else {
+			bv := a.Float()
+			for i, v := range a.Float() {
+				c.Append(v + bv[i])
+			}
+		}
+	default:
+		c = series.Ints([]int64{})
+		if la >= lb {
+			av := a.Int64()
+			for i, v := range b.Int64() {
+				c.Append(av[i] - v)
+			}
+		} else {
+			bv := a.Int64()
+			for i, v := range a.Int64() {
+				c.Append(v + bv[i])
 			}
 		}
 	}
--- mat/matrix_test.go
+++ mat/matrix_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/dreamsxin/gota/series"
 )
 
-func TestMatrix_Dot(t *testing.T) {
+func TestMatrix_Mul(t *testing.T) {
 	table := []struct {
 		a        series.Series
 		b        series.Series
@@ -24,7 +24,35 @@ func TestMatrix_Dot(t *testing.T) {
 		b := test.b
 
 		expected := test.expected.Records()
-		received := Dot(a, b).Records()
+		received := Mul(a, b).Records()
+		t.Log(received)
+		if !reflect.DeepEqual(expected, received) {
+			t.Errorf(
+				"Test:%v\nExpected:\n%v\nReceived:\n%v",
+				testnum, expected, received,
+			)
+		}
+	}
+}
+
+func TestMatrix_Div(t *testing.T) {
+	table := []struct {
+		a        series.Series
+		b        series.Series
+		expected series.Series
+	}{
+		{
+			series.Ints([]int{0, 2, 1, 5, 9}),
+			series.Ints([]int{0, 1, 2, 2, 4}),
+			series.Floats([]float64{0, 2, 0.5, 2.5, 2.25}),
+		},
+	}
+	for testnum, test := range table {
+		a := test.a
+		b := test.b
+
+		expected := test.expected.Records()
+		received := Div(a, b).Records()
 		t.Log(received)
 		if !reflect.DeepEqual(expected, received) {
 			t.Errorf(
